d733feb4652362a267a7e1eb60904e1b143a0300
AmerikeAzul14 <nikot92089@upshopt.com>
2022-08-30 16:46:30 -0500

    Cannon LookAt

--- TowerDefenseAR/Assets/_Code/CannonController.cs
+++ TowerDefenseAR/Assets/_Code/CannonController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class CannonController : MonoBehaviour
@@ -15,20 +16,42 @@ public class CannonController : MonoBehaviour
 
     private void Update()
     {
-        LookAtEnemey();
+        LookAt(_YawPoint, Enemy,'y');
+        LookAt(_PitchPoint, Enemy, 'a');
     }
 
-    private void LookAtEnemey()
+    private void LookAt(GameObject looker, GameObject target, char axis)
     {
-        Vector3 targetPositionHorizontal = _YawPoint.transform.position - Enemy.transform.position;
-        targetPositionHorizontal.y = 0;
-        var yawRotation = Quaternion.LookRotation(targetPositionHorizontal);
-        transform.rotation = Quaternion.Slerp(_YawPoint.transform.rotation, yawRotation, _SpeedDamp * Time.deltaTime);
+        Vector3 targetVector = target.transform.position - looker.transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(targetVector);
+        Vector3 rotation = lookRotation.eulerAngles;
+        switch (axis)
+        {
+            case 'x':
+                rotation.y = 0;
+                rotation.z = 0;
+                break;
 
-        Vector3 targetPositionVertical = _PitchPoint.transform.position - Enemy.transform.position;
-        targetPositionVertical.x = 0;
-        var pitchRotation = Quaternion.LookRotation(targetPositionVertical);
-        transform.rotation = Quaternion.Slerp(_PitchPoint.transform.rotation, pitchRotation, _SpeedDamp * Time.deltaTime);
+            case 'y':
+                rotation.x = 0;
+                rotation.z = 0;
+                break;
+
+            case 'z':
+                rotation.x = 0;
+                rotation.y = 0;
+                break;
+
+            case 'a':
+                break;
+
+            default:
+                Debug.Log("Incorrect axis or casing typed");
+                return;
+                break;
+        }
+       
+        looker.transform.rotation = Quaternion.Euler(rotation);
     }
 
 
