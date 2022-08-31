85c246b125645221dd7c36bfcd254ac098f20519
Diana Ramos/ Dinorekkos <diana_ramos_g@hotmail.com>
2022-08-31 16:51:14 -0500

    replace pos obj

--- TowerDefenseAR/Assets/_Code/ImageIdentifier.cs
+++ TowerDefenseAR/Assets/_Code/ImageIdentifier.cs
@@ -1,33 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class ImageIdentifier : MonoBehaviour
 {
+    public List<GameObject> _gameObjects;
+    public Text reference; 
+    
     private ARTrackedImageManager _TrackerManager;
 
-    private void Start()
+    private void Awake()
     {
         _TrackerManager = GetComponent<ARTrackedImageManager>();
     }
 
     private void OnEnable()
     {
-        _TrackerManager.trackedImagesChanged += OnImageChange;
+        _TrackerManager.trackedImagesChanged += UpdatedImage;
     }
 
     private void OnDisable()
     {
-        _TrackerManager.trackedImagesChanged -= OnImageChange;
+        _TrackerManager.trackedImagesChanged -= UpdatedImage;
     }
 
-    private void OnImageChange(ARTrackedImagesChangedEventArgs arg)
+    
+    private void UpdatedImage(ARTrackedImagesChangedEventArgs args)
     {
-        foreach (var item in arg.added)
+        _gameObjects.Add(GameObject.FindGameObjectWithTag("TrackedImage"));
+
+        foreach (var item in args.added)
         {
-            Debug.Log(item.referenceImage.name);
+            
+            switch (item.referenceImage.name)
+            {
+                case "Tower":
+                    foreach (var obj in _gameObjects)
+                    {
+                        if (obj.name == "Tower")
+                        {
+                            PositionsController pos = obj.GetComponent<PositionsController>();
+                            pos.RePostionGO(item);
+                        }
+                    }
+                    
+                break;
+             // case "imagen 2":
+            // animalPrefab = spawnedPrefabs["imagen 2"];
+            // animalPrefab.transform.position = trackedImage.transform.position;
+            // animalPrefab.SetActive(true);
+            // break;
+            }
+             
         }
+    
     }
 }
